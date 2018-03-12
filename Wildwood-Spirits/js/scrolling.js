ae2d91b481938d7e3264579a39705b9cb0d849d0
Sean C. Kanaley <SeanKanaley@gmail.com>
2018-03-12 15:00:16 -0700

    Work from HTML-CSS-2
    Work creating websites with various front end Frameworks.

---
+++ Wildwood-Spirits/js/scrolling.js
@@ -0,0 +1,31 @@
+﻿
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    $('nav a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+	
+	 $('.down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+	
+	$(window).stellar();
+	
+	new WOW().init();
+	
+
+});
